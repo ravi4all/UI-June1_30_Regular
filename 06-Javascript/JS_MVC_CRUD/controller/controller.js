95ce227122036daf1ecb671a9cf3a42353964b00
ravi4all <ravikanttyagi.2295@gmail.com>
2017-07-17 15:28:01 +0530

    Add files via upload

--- 06-Javascript/JS_MVC_CRUD/controller/controller.js
+++ 06-Javascript/JS_MVC_CRUD/controller/controller.js
@@ -6,12 +6,31 @@ function init(){
     desc = document.getElementById("box_3");
     document.getElementById("add").addEventListener("click",add_avenger);
     document.getElementById("delete").addEventListener("click",delete_avenger);
+    document.getElementById("save").addEventListener("click",save_avenger);
+    document.getElementById("load").addEventListener("click",load_avenger);
+}
+
+function save_avenger(){
+    if(window.localStorage){
+        var json = JSON.stringify(avenger_obj.avenger_list);
+        /*console.log(json);*/
+        localStorage.setItem('avenger_data',json)
+    }
+}
+
+function load_avenger(){
+    if(window.localStorage){
+        var json = JSON.parse(localStorage.avenger_data);
+        /*console.log(json);*/
+        avenger_obj.avenger_list = json;
+        print_avenger()
+    }
 }
 
 function add_avenger(){
-    ul = document.getElementById("item_list");
-    li = document.createElement("li");
-    img = document.createElement("img");
+    var ul = document.getElementById("item_list");
+    var li = document.createElement("li");
+    var img = document.createElement("img");
     img.src = 'garbage.png';
 
     avenger_obj.add_avenger(av_name.value,strength.value,desc.value);
@@ -27,9 +46,26 @@ function add_avenger(){
 function selected(){
     event.srcElement.classList.toggle("selected");
     var current_element = event.srcElement.innerHTML.split(" ")[0];
-    console.log(current_element);
+    /*console.log(current_element);*/
+    avenger_obj.toggle_avenger(current_element);
 }
 
 function delete_avenger(){
+    avenger_obj.delete_avenger();
+    print_avenger();
+}
+
+function print_avenger(){
+    var ul = document.getElementById("item_list");
+    ul.innerHTML = "";
+    avenger_obj.avenger_list.forEach(function(obj){
+        var li = document.createElement("li");
+        var img = document.createElement("img");
+        img.src = 'garbage.png';
+        li.innerHTML = obj.id + " " + obj.name + " " + obj.strength + " " + obj.desc;
+        ul.appendChild(li);
+        li.appendChild(img);
+        li.addEventListener("click",selected);
+    })
 
 }
--- 06-Javascript/JS_MVC_CRUD/model/avenger_obj.js
+++ 06-Javascript/JS_MVC_CRUD/model/avenger_obj.js
@@ -7,6 +7,21 @@ var avenger_obj = {
         var obj = new Avenger(this.id,name,strength,desc);
         this.avenger_list.push(obj);
         console.log(this.avenger_list);
+    },
+
+    toggle_avenger : function(id){
+        var selected_avenger = this.avenger_list.filter(function(obj){
+            return obj.id == id;
+        })
+        /*console.log(selected_avenger);*/
+        selected_avenger[0].selected = !selected_avenger[0].selected;
+        console.log(selected_avenger);
+    },
+
+    delete_avenger : function(){
+        this.avenger_list = this.avenger_list.filter(function(obj){
+            return obj.selected == false;
+        })
     }
 
 }
